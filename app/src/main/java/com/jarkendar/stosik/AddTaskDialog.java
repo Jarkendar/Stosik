b571ef021b1e70f00a53888d3ae0ee6589534dd4
jaroslaw <jarek.skrzypczak@o2.pl>
2018-12-31 18:06:37 +0100

    Add class serve add dialog

---
+++ app/src/main/java/com/jarkendar/stosik/AddTaskDialog.java
@@ -0,0 +1,55 @@
+package com.jarkendar.stosik;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class AddTaskDialog extends Dialog implements View.OnClickListener {
+
+    private Context context;
+    private EditText titleEdit, priorityEdit, endDateEdit;
+    private Button addButton, cancelButton;
+
+    public AddTaskDialog(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.add_task_layout_dialog);
+        titleEdit = (EditText) findViewById(R.id.editText_title);
+        priorityEdit = (EditText) findViewById(R.id.editText_priority);
+        endDateEdit = (EditText) findViewById(R.id.editText_end_date);
+        addButton = (Button) findViewById(R.id.button_add);
+        cancelButton = (Button) findViewById(R.id.button_cancel);
+        addButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_add:{
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+                DatabaseLackey databaseLackey = new DatabaseLackey(context);
+                try {
+                    databaseLackey.insertTask(databaseLackey.getWritableDatabase(), new Task(titleEdit.getText().toString(), Integer.parseInt(priorityEdit.getText().toString()), simpleDateFormat.parse(endDateEdit.getText().toString().trim())));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+        dismiss();
+    }
+}
